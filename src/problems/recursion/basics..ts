cd5b14c6efb4997ccffd2b3b0d9f7eaf477baaa3
Aashish <aashishdubey.dev@gmail.com>
2025-11-15 02:34:10 +0530

    solved basic recursion

---
+++ src/problems/recursion/basics..ts
@@ -0,0 +1,32 @@
+export const printNameNTimes = (n: number, name: string): any => {
+  if (n == 0) return;
+  console.log(name);
+  return printNameNTimes(n - 1, name);
+};
+
+export const print1ToN = (count: number, n: number): any => {
+  if (count > n) return;
+  console.log(count++);
+  return print1ToN(count, n);
+};
+
+export const printNTo1 = (n: number): any => {
+  if (n === 0) return;
+  console.log(n);
+  return printNTo1(n - 1);
+};
+
+export const sumOfNNumbers = (sum: number, n: number): any => {
+  if (n === 0) return sum;
+  return sumOfNNumbers(sum + n, n - 1);
+};
+
+export const sumOfNNumbers2 = (n: number): any => {
+  if (n === 0) return;
+  return n + sumOfNNumbers2(n - 1);
+};
+
+export const factorial = (n: number): any => {
+  if (n < 1) return;
+  return n * factorial(n - 1);
+};
